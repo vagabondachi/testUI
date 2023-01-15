bb2ed269a29eb99f3bed441298b4b671236e6038
jmgb27 <mark.john6672@gmail.com>
2023-01-16 00:17:25 +0800

    unique username

--- src/renderer/components/Registration.jsx
+++ src/renderer/components/Registration.jsx
@@ -18,33 +18,55 @@ function Registration() {
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
   const months = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
   ];
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    const displayNameRef = firebase
+      .firestore()
+      .collection('users')
+      .where('displayName', '==', username);
 
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then((user) => {
-        user.user
-          .updateProfile({
-            displayName: username,
+    displayNameRef.get().then((doc) => {
+      if (!doc.empty) {
+        setError('username already exists, try a different one.');
+      } else {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then((user) => {
+            user.user
+              .updateProfile({
+                displayName: username,
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+            firebase.firestore().collection('users').doc(user.user.uid).set({
+              userId: user.user.uid,
+              userLanguage: 'en',
+              translateToLanguage: 'en',
+              displayName: username,
+            });
           })
           .catch((error) => {
+            setError(error.message);
             console.log(error);
           });
-        firebase.firestore().collection('users').doc(user.user.uid).set({
-          userId: user.user.uid,
-          userLanguage: 'en',
-          translateToLanguage: 'en',
-        });
-      })
-      .catch((error) => {
-        setError(error.message);
-        console.log(error);
-      });
+      }
+    });
   };
 
   useEffect(() => {
@@ -98,36 +120,59 @@ function Registration() {
             </div>
           </div>
           <br />
-        <div className="container">
+          <div className="container">
             <label>Date of Birth</label>
             <br />
-            <span className='month-container'>
-              <select name="month" value={month} onChange={e => setMonth(e.target.value)}>
+            <span className="month-container">
+              <select
+                name="month"
+                value={month}
+                onChange={(e) => setMonth(e.target.value)}
+              >
                 <option value="" selected disabled>
                   Month
                 </option>
                 {months.map((month, index) => (
-                  <option key={index} value={index + 1}>{month}</option>
+                  <option key={index} value={index + 1}>
+                    {month}
+                  </option>
                 ))}
               </select>
             </span>
-            <span className='days-container'>
-              <select name="day" id="day" value={day} onChange={e => setDay(e.target.value)}>
+            <span className="days-container">
+              <select
+                name="day"
+                id="day"
+                value={day}
+                onChange={(e) => setDay(e.target.value)}
+              >
                 <option value="" selected disabled>
                   Day
                 </option>
-                {Array.from({length: 31}, (_,i) => i+1).map(day => (
-                  <option key={day} value={day}>{day}</option>
+                {Array.from({ length: 31 }, (_, i) => i + 1).map((day) => (
+                  <option key={day} value={day}>
+                    {day}
+                  </option>
                 ))}
               </select>
             </span>
-            <span className='year-container'>
-              <select name="year" id="year" value={year} onChange={e => setYear(e.target.value)}>
+            <span className="year-container">
+              <select
+                name="year"
+                id="year"
+                value={year}
+                onChange={(e) => setYear(e.target.value)}
+              >
                 <option value="" selected disabled>
                   Year
                 </option>
-                {Array.from({length: new Date().getFullYear()-1900}, (_,i) => i+1901).map(year => (
-                  <option key={year} value={year}>{year}</option>
+                {Array.from(
+                  { length: new Date().getFullYear() - 1900 },
+                  (_, i) => i + 1901
+                ).map((year) => (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
                 ))}
               </select>
             </span>
